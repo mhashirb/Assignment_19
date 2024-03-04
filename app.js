65638a2b46d877df2afaa9c53ed83cd645dbd6a0
mhashirb <hashirbaig275@gmail.com>
2024-03-04 02:19:21 -0800

    Assignment 19

---
+++ app.js
@@ -0,0 +1,77 @@
+
+//Chapter 15
+// Question 1
+// var emptyArray=[];
+
+// Question 2
+// var array=["string1"];
+
+// Question 3
+// var alphabet=["h","i","j","k"];
+// alert(alphabet[2]);
+
+// Question 4
+// var alphabet=["h","i","j","k","l","m","n","o"];
+// document.write(alphabet.length);
+
+// Question 5
+// var pets=[];
+// pets[0]="dogs";
+// pets[1]="cats";
+// alert(""+pets[1]);
+
+
+// Chapter 16
+
+// Question 1
+// var arr=["string1"];
+// arr.push("string2");
+// alert(arr[1]);
+
+// Question 2
+// var Alphabet=["h","i","j","k"];
+// Alphabet.pop();
+// alert(Alphabet[3]);
+
+// Question 3
+// var Alphabet=["h","i","j","k"];
+// Alphabet.push(1);
+// alert(Alphabet.pop());
+
+// Question 4
+// var sizes=["S","M","XL","XXL","XXXL"];
+// alert(sizes.shift());
+
+// Question 5
+// var sizes=["S","M","XL","XXL","XXXL"];
+// sizes.unshift("XXXS","XXS","XS");
+
+// Question 6
+// var cars=["Toyota"];
+// cars.unshift("Mercedes");
+// alert(cars.shift());
+
+// Question 7
+// var sizes=["S","M","XL","XXL","XXXL"];
+// sizes.splice(2,0,"L");
+// alert(sizes.splice(2,1));
+
+// Question 8
+// var sizes=["S","M","XL","XXL","XXXL"];
+// var regSize=sizes.slice(0,3);
+// alert(regSize[3]);
+
+// Question 9
+// var pets=["dogs","cats","ox","duck","frog"];
+// pets.splice(1,3,"fish","cow");
+// alert(pets);
+
+// Question 10
+// var pets=["dogs","cats","ox","duck","frog"];
+// pets.splice(1,2);
+// alert(pets);
+
+//Question 11
+var pets=["dogs","cats","ox","duck","frog","flea"];
+pets=pets.slice(3,5);
+alert(pets);
